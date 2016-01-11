9b8f11724a3d80d6ca76d23a8d66834426afda73
nn1098 <neilnotman@hotmail.com>
2016-01-11 17:05:04 +0000

    joystick controlls

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -2,14 +2,19 @@
 
 using namespace std;
 
-static const string textureNames[]{
-    "spaceship1.png",        "spaceship2.png",        "spaceship3.png",
-    "Spaceship-Drakir1.png", "Spaceship-Drakir2.png", "Spaceship-Drakir3.png",
-    "Spaceship-Drakir4.png", "Spaceship-Drakir5.png", "Spaceship-Drakir6.png",
-    "Spaceship-Drakir7.png",
-};
+static const string textureNames[]{"spaceship1.png",
+                                   "spaceship2.png",
+                                   "spaceship3.png",
+                                   "Spaceship-Drakir1.png",
+                                   "Spaceship-Drakir2.png",
+                                   "Spaceship-Drakir3.png",
+                                   "Spaceship-Drakir4.png",
+                                   "Spaceship-Drakir5.png",
+                                   "Spaceship-Drakir6.png",
+                                   "Spaceship-Drakir7.png",
+                                   "bullet.png"};
 
-static sf::Texture *textures[10];
+static sf::Texture *textures[11];
 
 static const string filepath = "..\\res/img/";
 
@@ -17,17 +22,20 @@ static sf::Sprite enemies[16];
 
 sf::Sprite playerSprite;
 sf::Texture *playerTexture;
-
+sf::Sprite bulletsprite;
+sf::Texture *bulletTexture;
 void Loadcontent() {
 
-  for (size_t i = 0; i < 10; i++) {
+  for (size_t i = 0; i < 11; i++) {
     textures[i] = new sf::Texture();
     if (!textures[i]->loadFromFile(filepath + textureNames[i])) {
-      throw invalid_argument("Ship not loaded!");
+      throw invalid_argument("Loading error!");
     }
   }
+
   playerSprite.setTexture(*textures[0]);
   playerSprite.setPosition(512, 256);
+  bulletsprite.setTexture(*textures[10]);
 
   for (size_t i = 0; i < 16; i++) {
     enemies[i].setTexture(*textures[(i % 7) + 3]);
@@ -44,42 +52,51 @@ void Update(sf::RenderWindow &window) {
     if (e.type == sf::Event::Closed)
       window.close();
 
-	//keyboard event handling
-	if (e.type == sf::Event::KeyPressed) {
-		if (e.key.code == sf::Keyboard::Escape)
-		{
-			window.close();
-		}
+    // keyboard event handling
+    if (e.type == sf::Event::KeyPressed) {
+      if (e.key.code == sf::Keyboard::Escape) {
+        window.close();
+      }
 
-		if (e.key.code == sf::Keyboard::W)
-		{
-			playerSprite.setPosition(playerSprite.getPosition().x, playerSprite.getPosition().y - tick);
-		}
+      if (e.key.code == sf::Keyboard::W) {
+        playerSprite.setPosition(playerSprite.getPosition().x,
+                                 playerSprite.getPosition().y - tick);
+      }
 
-		if (e.key.code == sf::Keyboard::S)
-		{
-			playerSprite.setPosition(playerSprite.getPosition().x, playerSprite.getPosition().y + tick);
-		}
-		if (e.key.code == sf::Keyboard::A)
-		{
-			playerSprite.setPosition(playerSprite.getPosition().x - tick, playerSprite.getPosition().y);
-		}
-		if (e.key.code == sf::Keyboard::D)
-		{
-			playerSprite.setPosition(playerSprite.getPosition().x + tick, playerSprite.getPosition().y);
-		}
-	}
+      if (e.key.code == sf::Keyboard::S) {
+        playerSprite.setPosition(playerSprite.getPosition().x,
+                                 playerSprite.getPosition().y + tick);
+      }
+      if (e.key.code == sf::Keyboard::A) {
+        playerSprite.setPosition(playerSprite.getPosition().x - tick,
+                                 playerSprite.getPosition().y);
+      }
+      if (e.key.code == sf::Keyboard::D) {
+        playerSprite.setPosition(playerSprite.getPosition().x + tick,
+                                 playerSprite.getPosition().y);
+      }
+    }
 
-	//joystick input
-	 if (sf::Joystick::isConnected(0))
-	{
-		float joystickX = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
-		float joystickY = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
+    // joystick input
+    if (sf::Joystick::isConnected(0)) {
+      float joystickX = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
+      float joystickY = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
 
-		playerSprite.setPosition(playerSprite.getPosition().x + (joystickX * 0.1), playerSprite.getPosition().y + (joystickY * 0.1));
+      playerSprite.setPosition(playerSprite.getPosition().x + (joystickX * 0.1),
+                               playerSprite.getPosition().y +
+                                   (joystickY * 0.1));
 
-	} 
+      // if the B button is pressed fire a bullet
+      if (e.JoystickButtonPressed) {
+        if (e.joystickButton.button == 0) {
+          bulletsprite.setPosition(playerSprite.getPosition().x + 30,
+                                   playerSprite.getPosition().y - 1);
+        }
+      }
 
+      bulletsprite.setPosition(bulletsprite.getPosition().x,
+                               bulletsprite.getPosition().y - tick);
+    }
   }
 
   for (size_t i = 0; i < 16; i++) {
@@ -92,6 +109,9 @@ void Render(sf::RenderWindow &window) {
   window.clear();
 
   window.draw(playerSprite);
+
+  window.draw(bulletsprite);
+
   for (size_t i = 0; i < 16; i++) {
     window.draw(enemies[i]);
   }
